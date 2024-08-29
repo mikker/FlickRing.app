2663b7aa9695a9a139fbe4dcacd01c554f89b1c2
Mikkel Malmberg <mikkel@brnbw.com>
2024-08-29 10:00:04 +0200

    Initial Commit

---
+++ FlickRing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlickRing
+//
+//  Created by Mikkel Malmberg on 29/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlickRing/FlickRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickRingApp.swift
+//  FlickRing
+//
+//  Created by Mikkel Malmberg on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlickRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
